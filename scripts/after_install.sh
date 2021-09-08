95a68f6e7a96d0c37747a141b5f5efe33c7bbf45
Chris Fidao <fideloper@gmail.com>
2021-09-08 14:54:25 -0500

    handle web server deploy vs queue server deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-chown -R cloudcasts:cloudcasts /home/cloudcasts/cloudcasts.io
-
+# Generate .env
 cd /home/cloudcasts/cloudcasts.io
 aws --region us-east-2 ssm get-parameter \
     --with-decryption \
@@ -9,6 +8,11 @@ aws --region us-east-2 ssm get-parameter \
     --output text \
     --query 'Parameter.Value' > .env
 
-chown cloudcasts:cloudcasts /home/cloudcasts/cloudcasts.io/.env
+# Set permissions
+chown -R cloudcasts:cloudcasts /home/cloudcasts/cloudcasts.io
 
-service php8.0-fpm reload
+# Reload php-fpm (clear opcache) if running
+systemctl is-active --quiet php8.0-fpm && service php8.0-fpm reload
+
+# Start supervisor jobs (if supervisor is used)
+systemctl is-active --quiet supervisor && supervisorctl start all
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# Stop supervisor jobs (if supervisor is used)
+systemctl is-active --quiet supervisor && supervisorctl stop all
+
 rm -rf /home/cloudcasts/cloudcasts.io
