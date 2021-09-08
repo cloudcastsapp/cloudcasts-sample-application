7624558b2f8c487c59105598c4ff461ee555edbf
Chris Fidao <fideloper@gmail.com>
2021-09-08 14:28:05 -0500

    move .env generation so it is not inside of build artifact but instead run during deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,13 @@
 
 chown -R cloudcasts:cloudcasts /home/cloudcasts/cloudcasts.io
 
+cd /home/cloudcasts/cloudcasts.io
+aws --region us-east-2 ssm get-parameter \
+    --with-decryption \
+    --name /cloudcasts/staging/env \
+    --output text \
+    --query 'Parameter.Value' > .env
+
+chown cloudcasts:cloudcasts /home/cloudcasts/cloudcasts.io/.env
+
 service php8.0-fpm reload
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,18 +5,10 @@ npm install
 npm run production
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
-# Generate a .env file
-aws --region us-east-2 ssm get-parameter \
-    --with-decryption \
-    --name /cloudcasts/staging/env \
-    --output text \
-    --query 'Parameter.Value' > .env
-
 # Create our build artifact
 git archive -o builds/cloudcasts.zip --worktree-attributes HEAD
 zip -qur builds/cloudcasts.zip vendor
 zip -qur builds/cloudcasts.zip public
-zip -qur builds/cloudcasts.zip .env
 
 # Upload artifact to s3
 aws s3 cp builds/cloudcasts.zip s3://cloudcasts-$1-artifacts/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip 
