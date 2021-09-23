d0620003caf92867cee402cfcb25ea82bac1038c
Chris Fidao <fideloper@gmail.com>
2021-09-23 08:41:05 -0500

    build and deploy staging/production

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-# Production assets/dependencies
-npm install
-npm run production
-composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+DO_BUILD="nah"
 
-# Create our build artifact
-git archive -o builds/cloudcasts.zip --worktree-attributes HEAD
-zip -qur builds/cloudcasts.zip vendor
-zip -qur builds/cloudcasts.zip public
 
-# Upload artifact to s3
-aws s3 cp builds/cloudcasts.zip s3://cloudcasts-artifacts/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
+if [[ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/main" ]]; then
+    DO_BUILD="yes"
+fi
+
+if [[ "$CODEBUILD_WEBHOOK_TRIGGER" == "tag/"* ]]; then
+    DO_BUILD="yes"
+fi
+
+
+if [[ "$DO_BUILD" == "yes" ]]; then
+    # Production assets/dependencies
+    npm install
+    npm run production
+    composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+    # Create our build artifact
+    git archive -o builds/cloudcasts.zip --worktree-attributes HEAD
+    zip -qur builds/cloudcasts.zip vendor
+    zip -qur builds/cloudcasts.zip public
+
+    # Upload artifact to s3
+    aws s3 cp builds/cloudcasts.zip s3://cloudcasts-artifacts/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
+fi
