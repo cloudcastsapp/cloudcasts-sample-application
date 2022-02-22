9e87f56a398b8bed2b04342e9b2ffd51132863a3
Bassem Shoukry <basem.shoukry@goodsmartegypt.com>
2022-02-22 17:13:08 +0200

    buildspec && build.sh files updated

--- scripts/before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Stop supervisor jobs (if supervisor is used)
-if [[ "$DEPLOYMENT_GROUP_NAME" == *"queue"* ]]; then
-    supervisorctl stop all
-fi
-
-rm -rf /home/cloudcasts/cloudcasts.io
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
 
-DO_BUILD="nah"
 
 
-if [[ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/main" ]]; then
-    DO_BUILD="yes"
-fi
+# Production assets/dependencies
 
-if [[ "$CODEBUILD_WEBHOOK_TRIGGER" == "tag/"* ]]; then
-    DO_BUILD="yes"
-fi
+npm install
+
+npm run production
+
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
 
-if [[ "$DO_BUILD" == "yes" ]]; then
-    # Production assets/dependencies
-    npm install
-    npm run production
-    composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
-    # Create our build artifact
-    git archive -o builds/cloudcasts.zip --worktree-attributes HEAD
-    zip -qur builds/cloudcasts.zip vendor
-    zip -qur builds/cloudcasts.zip public
+# Generate a .env file
 
-    # Upload artifact to s3
-    aws s3 cp builds/cloudcasts.zip s3://cloudcasts-artifacts/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
-fi
+aws --region eu-central-1 ssm get-parameter \
+     --with-decryption \
+     --name /cloudcasts/staging/env \
+     --output text \
+     --query 'Parameter.Value' > .env
+
+
+# Create our build artifact
+
+git archive -o builds/cloudcasts.zip --worktree-attributes HEAD
+
+zip -qur builds/cloudcasts.zip vendor
+
+zip -qur builds/cloudcasts.zip public
+
+zip -qur builds/cloudcasts.zip .env # Grab our testing .env file for now
+
+
+
+# Upload artifact to s3
+
+aws s3 cp builds/cloudcasts.zip s3://cloudcasts-$1-artifacts/"$CODEBUILD_RESOLVED_SOURCE_VERSION".zip
